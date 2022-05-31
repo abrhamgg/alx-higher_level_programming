df1b78fc67bad7ca72082ac06d383826d0b428f0
abrhamgg <abrhamgg@gmail.com>
2022-05-31 14:02:24 +0300

    added shebang

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 for c in range(97, 123):
     if chr(c) == 'q' or chr(c) == 'e':
         continue
--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 for i in range(99):
     value = hex(i)
     print(f"{i} = {value}")
