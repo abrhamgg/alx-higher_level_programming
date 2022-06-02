05aad0322e9736809736150dc4cf7e8e108672f9
abrhamgg <abrhamgg@gmail.com>
2022-06-02 21:21:38 +0300

    argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
-from sys import argv
-n = len(argv)
-print("{:d}: arguments".format(n - 1))
-for i in range(1, n):
-    print("{:d}: {:s}".format(i, argv[i]))
-    if not argv[i]:
-        break
+def print_arg(argv):
+    n = len(argv) - 1
+    if n == 0:
+        print("{:d} argument.".format(n))
+        return
+    else:
+        if n == 1:
+            print("{:d} argument:".format(n))
+        else:
+            print("{:d} arguments:".format(n))
+        i = 1
+        while i <= n:
+            print("{:d}: {:s}".format(i, argv[i]))
+            i += 1
+
+if __name__ == "__main__":
+    import sys
+    print_arg(sys.argv)
