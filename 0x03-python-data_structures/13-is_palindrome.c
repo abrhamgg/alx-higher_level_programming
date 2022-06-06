028ce6cc96c5c2d98a3cd80297e295cae3906d4d
abrhamgg <abrhamgg@gmail.com>
2022-06-06 23:00:09 +0300

    fixed

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,74 +1,51 @@
+#include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "lists.h"
 
 /**
- * print_listint - prints all elements of a listint_t list
- * @h: pointer to head of list
- * Return: number of nodes
+ * is_palindrome - function that checks if a list is palindrome
+ * @head: pointer to linked list
+ * Return: 0 if not palindrome and 1 if its palindrome
  */
-size_t print_listint(const listint_t *h)
+
+int is_palindrome(listint_t **head)
 {
-    const listint_t *current;
-    unsigned int n; /* number of nodes */
+	listint_t *current = *head;
+	listint_t *fast = *head;
+	listint_t *new = *head;
+	int k  = 0;
+	int n = 0;
 
-    current = h;
-    n = 0;
-    while (current != NULL)
-    {
-        printf("%i\n", current->n);
-        current = current->next;
-        n++;
-    }
+	if (!*head)
+		return (0);
+	while (current != NULL)
+	{
+		current = current->next;
+		k++;
+		n++;
+	}
+	int i = 0;
+	int count = 0;
 
-    return (n);
-}
-
-/**
- * add_nodeint_end - adds a new node at the end of a listint_t list
- * @head: pointer to pointer of first node of listint_t list
- * @n: integer to be included in new node
- * Return: address of the new element or NULL if it fails
- */
-listint_t *add_nodeint_end(listint_t **head, const int n)
-{
-    listint_t *new;
-    listint_t *current;
-
-    current = *head;
-
-    new = malloc(sizeof(listint_t));
-    if (new == NULL)
-        return (NULL);
-
-    new->n = n;
-    new->next = NULL;
-
-    if (*head == NULL)
-        *head = new;
-    else
-    {
-        while (current->next != NULL)
-            current = current->next;
-        current->next = new;
-    }
-
-    return (new);
-}
-
-/**
- * free_listint - frees a listint_t list
- * @head: pointer to list to be freed
- * Return: void
- */
-void free_listint(listint_t *head)
-{
-    listint_t *current;
-
-    while (head != NULL)
-    {
-        current = head;
-        head = head->next;
-        free(current);
-    }
+	while (new != NULL)
+	{
+		while (i < k - 1)
+		{
+			fast = fast->next;
+			i++;
+		}
+		if (count == n / 2)
+			return (1);
+		if (new->n == fast->n)
+		{
+			k--;
+			new = new->next;
+			fast = *head;
+			i = 0;
+			count = count + 1;
+		}
+		else
+			return (0);
+	}
+	return (0);
 }
