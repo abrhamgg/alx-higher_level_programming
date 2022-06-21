3ef35e1daab2c8ef3f52a66bbd2fcb9991e1287e
abrhamgg <abrhamgg@gmail.com>
2022-06-21 20:38:14 +0300

    intialize a class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+    def __init__(self, __size):
+        """initialize the Square class"""
+        self.__size = __size
+        pass
