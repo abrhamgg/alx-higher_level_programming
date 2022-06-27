8d50badddf404acff695f3d8c533dbaa098afe66
abrhamgg <abrhamgg@gmail.com>
2022-06-27 20:33:01 +0300

    getter and setters

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -15,8 +15,8 @@ class Rectangle:
             width: width of triangle
             height: height of triangle
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
