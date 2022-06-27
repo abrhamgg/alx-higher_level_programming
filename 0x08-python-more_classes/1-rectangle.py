168741c958ff1117d649548cce4dfc3c79e223ad
abrhamgg <abrhamgg@gmail.com>
2022-06-27 20:01:12 +0300

    getter and setters

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -14,24 +14,29 @@ class Rectangle:
 
     @property
     def width(self, value):
+        """
+        get rectangle width
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
-        """check if the value to be set for width is apropirate"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
+        self.__width = value
 
     @property
     def height(self, value):
+        """rectangle height
+        """
         return self.__width
 
     @height.setter
     def height(self, value):
-        """check if the value to be set for height is apprpirate"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
+        self.__height = value
