34e771953a047ba1698e181cdee4e13702d7ca20
abrhamgg <abrhamgg@gmail.com>
2022-06-28 22:42:24 +0300

    everything

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+module: say_my_name
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """print first and last name
+    first_name -- string
+    last_name -- string
+    """
+    #  ERROR MESSAGE DICT  #
+    err_msg = {}
+    err_msg["FirstNotStr"] = "first_name must be a string"
+    err_msg["LastNotStr"] = "last_name must be a string"
+
+    #  TESTS  #
+    if type(first_name) != str:
+        raise TypeError(err_msg["FirstNotStr"])
+    if type(last_name) != str:
+        raise TypeError(err_msg["LastNotStr"])
+
+    print("My name is {}".format(first_name), end='')
+    if len(last_name) == 0:
+        print()
+    else:
+        print(" {}".format(last_name))
