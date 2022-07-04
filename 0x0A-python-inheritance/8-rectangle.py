12604f5eb6c7870f9a57ed5123bda7b56e584ff2
abrhamgg <abrhamgg@gmail.com>
2022-07-04 23:14:21 +0300

    inherit

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Empty class
+"""
+
+
+class BaseGeometry:
+    """Base Geometry"""
+    def area(self):
+        """Not yet implemented"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validate parameter as integer:
+        Args:
+            name (str): the name of the parameter
+            value (int): The parameter to validate
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """inherits from BaseGeometry"""
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        BaseGeometry.integer_validator(self, "width", self.__width)
+        BaseGeometry.integer_validator(self, "height", self.__height)
