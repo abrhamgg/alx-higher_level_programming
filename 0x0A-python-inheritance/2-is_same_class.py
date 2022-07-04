5b26ba9d6719c8494e144de3cee1bde55734e2f2
abrhamgg <abrhamgg@gmail.com>
2022-07-04 20:26:01 +0300

    type()

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+is_same_class
+"""
+
+
 def is_same_class(obj, a_class):
     """function to check if an object is an exact instant of class"""
     if type(obj) == a_class:
