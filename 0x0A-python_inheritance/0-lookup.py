b5f61fdd9f3493786e537cd5fb690d5d041020e3
abrhamgg <abrhamgg@gmail.com>
2022-07-04 18:21:36 +0300

    fix

--- 0x0A-python_inheritance/0-lookup.py
+++ 0x0A-python_inheritance/0-lookup.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+function that returns the list of available methods of an object
+"""
+
+
 def lookup(obj):
     """function that returns all available method of an object"""
     return dir(obj)
