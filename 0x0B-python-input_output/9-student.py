6d3e9fa80252f3babc07cab5caacc1aa169145a2
abrhamgg <abrhamgg@gmail.com>
2022-07-05 21:16:49 +0300

    class Stu

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -7,6 +7,11 @@ student class
 class Student:
     """Student class"""
     def __init__(self, first_name, last_name, age):
+        """ intialize this class with
+        first_name
+        last_name
+        age
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
