f3da98e9b0e80c6933488dee1b342b32a4eeeb30
abrhamgg <abrhamgg@gmail.com>
2022-07-05 20:14:23 +0300

    json dumps

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -6,5 +6,10 @@ To JSON string
 
 
 def to_json_string(my_obj):
+    """
+    function the changes a python object to json string
+    my_obj: input obj
+    return: json string
+    """
     jn = json.dumps(my_obj)
     return jn
