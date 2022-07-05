26a1fe9314646f2a8864777a9275de21924392ee
abrhamgg <abrhamgg@gmail.com>
2022-07-05 15:03:23 +0300

    f.write

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" function that write a string to text file"""
+
+def write_file(filename="", text=""):
+    """
+    function that writes string to a text file and returns the number
+    of characters written
+    :param filename: input file
+    :param text: the text to be written
+    :return: number of chars
+    """
+    with open(filename, mode="w", encoding='utf-8') as f:
+        f.write(text)
+        return len(text)
