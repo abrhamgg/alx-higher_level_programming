7057003f0109b8eb34097fb00ebf2c067afd7fda
abrhamgg <abrhamgg@gmail.com>
2022-07-05 21:13:33 +0300

    class Stu

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,19 @@
+#!/bin/bash/python3
+"""
+student class
+"""
+
+
+class Student:
+    """Student class"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """retrieves dictionary representation 
+        of python object
+        self: object
+        """
+        return self.__dict__
