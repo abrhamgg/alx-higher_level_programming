baf9489cd9d31a3ca76691af83e62f3e5522d264
abrhamgg <abrhamgg@gmail.com>
2022-08-10 19:00:41 +0300

    state

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
--- creates the database hbtn_0d_usa and the table states
-CREATE DATABASE `hbtn_0d_usa`;
-USE `hbtn_0d_usa`;
-CREATE TABLEM IF NOT EXISTS `states` (
-	`id` INT AUTO_INCREMENT NOT NULL PRIMARY_KEY,
-	`name` VARCHAR(256)
-	);
+-- Creates the table hbtn_0d_usa with table states.
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
+    PRIMARY KEY(`id`),
+    `id`   INT          NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(256) NOT NULL
+);
