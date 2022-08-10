c2d8f7c8b5dbc6210fbee45008b6d944e667b8bb
abrhamgg <abrhamgg@gmail.com>
2022-08-10 17:44:51 +0300

    grant access

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- create user with password
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT *.* TO 'user_0d_1';
+GRANT ALL PRIVILEGES *.* TO 'user_0d_1'@'localhost';
