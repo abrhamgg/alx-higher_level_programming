8943545da05893c78187fce559ba9f83acb97c44
abrhamgg <abrhamgg@gmail.com>
2022-09-05 23:56:46 +0300

    process.stdout.write

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const myArgs = process.argv;
+if (!myArgs[2]) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < myArgs[2]; i++) {
+    for (let j = 0; j < myArgs[2]; j++) {
+      process.stdout.write('X');
+    }
+    console.log();
+  }
+}
