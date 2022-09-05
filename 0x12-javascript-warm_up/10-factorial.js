3c90b0fac224c8885f00851d936b8c252320acde
abrhamgg <abrhamgg@gmail.com>
2022-09-06 00:32:46 +0300

    factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const myArgs = process.argv;
 function factorial (a) {
-if (a === 0 || a === 1 ) {
-     return (1);
-}
-return (a * factorial(a - 1));
+  if (a === 0 || a === 1) {
+    return (1);
+  }
+  return (a * factorial(a - 1));
 }
 if (!myArgs[2]) {
   console.log(1);
