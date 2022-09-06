54fce1dee25b39c87850e2eaffab12d25ccea698
abrhamgg <abrhamgg@gmail.com>
2022-09-07 00:34:26 +0300

    reverse array

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,22 +1,7 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  let len = list.length - 1;
-  let temp = 0;
-  console.log(len);
-  if (len % 2 === 0) {
-    for (let i = 0; i <= list.length / 2; i++) {
-      temp = list[i];
-      list[i] = list[len];
-      list[len] = temp;
-      len--;
-    }
-  } else {
-    for (let i = 0; i < list.length / 2; i++) {
-      temp = list[i];
-      list[i] = list[len];
-      list[len] = temp;
-      len--;
-    }
-  }
-  return list;
+  return list.reduceRight(function(previous, current) {
+    previous.push(current);
+    return previous;
+  }, []);
 };
