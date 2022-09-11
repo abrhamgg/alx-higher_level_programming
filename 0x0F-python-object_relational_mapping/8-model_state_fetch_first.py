cacd9c41d0e4ae6fb795c3aa7e9cf49abffd9a7f
abrhamgg <abrhamgg@gmail.com>
2022-09-11 13:42:55 +0300

    State mode

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+script that prints the first State object from
+the database
+"""
+import sys
+
+from sqlalchemy import create_engine
+from model_state import State, Base
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    engine = create_engine('mysql://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(engine)
+    session = Session()
+    first = session.query(State).first()
+    print('{}: {}'.format(first.id, first.name))
