4a785ac651740c3400c5fa61bdd10657d1a61188
abrhamgg <abrhamgg@gmail.com>
2022-09-11 08:54:10 +0300

    safe from injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -14,9 +14,9 @@ if __name__ == "__main__":
         if ';' in sys.argv[4]:
             new = sys.argv[4].split(';')
             checked_sql = new[0].strip('\"')
-            print(checked_sql)
         else:
             checked_sql = sys.argv[4]
         print(checked_sql)
-    r.execute("SELECT * FROM states WHERE name LIKE '{}' ORDER BY `id` ASC".format(checked_sql))
+    r.execute("SELECT * FROM states WHERE name LIKE"
+              "'{}' ORDER BY `id` ASC".format(checked_sql))
     [print(state) for state in r.fetchall() if state[1] in checked_sql]
