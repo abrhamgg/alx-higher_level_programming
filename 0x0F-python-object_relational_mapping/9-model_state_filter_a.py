97418959f0826e175c3ddcd8e2d336b02088983f
abrhamgg <abrhamgg@gmail.com>
2022-09-11 13:57:33 +0300

    contains a

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+script that lists all State objects from the database
+"""
+import sys
+from sqlalchemy import MetaData, create_engine
+from model_state import State, Base
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(engine)
+    session = Session()
+    for state in session.query(State).order_by(State.id):
+        if 'a' in state.name:
+            print('{}: {}'.format(state.id, state.name))
