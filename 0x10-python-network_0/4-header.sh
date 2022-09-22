8e86a795d6bbdfcb1e880064ac2294c5b4df02be
abrhamgg <abrhamgg@gmail.com>
2022-09-23 00:37:17 +0300

    revert

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # curl sends GET req to URL, displays response body
-echo -n $(curl -sH -X GET "X-School-User-Id: 98" "$1") | echo Hello School!
+echo -n $(curl -sH -X GET "X-School-User-Id: 98" "$1")
