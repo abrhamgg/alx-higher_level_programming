ca810c3ff040a9a9a6a6fd65f5da533d297b80a1
abrhamgg <abrhamgg@gmail.com>
2022-09-23 17:09:29 +0300

    requests

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,7 @@ Python script that fetches https://alx-intranet.hbtn.io/status
 """
 import requests
 
-r = requests.get('http://abrhamgg.tech')
+r = requests.get('https://alx-intranet.hbtn.io/status')
 print("Body response:")
 print("\t - type: {}".format(type(r.__dict__['_content'].decode('utf-8'))))
 print("\t - content: {}".format(r.__dict__['_content'].decode('utf-8')))
