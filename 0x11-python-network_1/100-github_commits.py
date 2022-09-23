35d1a2d250a997354cbb8939c800ebb2b4f12493
abrhamgg <abrhamgg@gmail.com>
2022-09-23 23:44:10 +0300

    challenge done

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Please list
+10 commits (from the most recent to oldest) of the repository
+"""
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    url = f"https://api.github.com/repos/{sys.argv[1]}/{sys.argv[2]}/commits"
+    value = "Accept: application/vnd.github+json"
+    r = requests.get(url, params=value)
+    res = r.json()
+    new = res[:10]
+    for i in new:
+        print("{} {}".format(i.get("sha"), i['commit']['author']['name']))
