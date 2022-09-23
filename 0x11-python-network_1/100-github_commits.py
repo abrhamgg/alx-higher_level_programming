325506f5651a26baf242ba047d6c3749304d3fec
abrhamgg <abrhamgg@gmail.com>
2022-09-24 00:11:09 +0300

    switched

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,7 +8,7 @@ import requests
 import sys
 
 if __name__ == '__main__':
-    url = f"https://api.github.com/repos/{sys.argv[1]}/{sys.argv[2]}/commits"
+    url = f"https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}/commits"
     r = requests.get(url)
     res = r.json()
     for i in res[:10]:
