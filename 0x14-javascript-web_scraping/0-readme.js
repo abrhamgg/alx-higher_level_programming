a474a13e52dbbe4dd720cd9dceda763f1677ef31
abrhamgg <abrhamgg@gmail.com>
2022-10-18 16:53:19 +0300

    hotfix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,6 +2,6 @@
 
 const fs = require('fs');
 const args = process.argv[2];
-fs.readFile(args[0], 'utf8', function (err, data) {
+fs.readFile(args[2], 'utf8', function (err, data) {
   if (data) { console.log(data); } else { console.log(err); }
 });
