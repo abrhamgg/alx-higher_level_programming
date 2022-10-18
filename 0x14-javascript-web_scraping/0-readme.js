60e7bf3993c197e948696e26147751b76436dd8c
abrhamgg <abrhamgg@gmail.com>
2022-10-18 14:28:21 +0300

    readme

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const args = process.argv.slice(2);
+fs.readFile(args[0], 'utf8', function (err, data) {
+  if (data) { console.log(data); } else { console.log(err); }
+});
